f653400d31aabc27bb0867dc45b9f42649b43aad
Marv_K254 <marvinkorir.mk@gmail.com>
2022-06-16 09:23:42 +0300

    added a a scheduler to the script

--- ec2-instance health check.py
+++ ec2-instance health check.py
@@ -1,4 +1,5 @@
 import boto3
+import schedule
 
 ec2_client = boto3.client('ec2')
 ec2_resource = boto3.resource('ec2')
@@ -11,10 +12,20 @@ reservations = ec2_client.describe_instances()
 #     for instance in instances:
 #         print(f"Instance {instance['InstanceId']} is {instance['State']['Name']}")
 
-statuses = ec2_client.describe_instance_status()
 
-for status in statuses["InstanceStatuses"]:
-    ins_status = status["InstanceStatus"] ["Status"]
-    sys_status = status["SystemStatus"]["Status"]
-    state = status["InstanceState"]["Name"]
-    print(f"Instance {status['InstanceId']} is {state} status is {ins_status} and system status is {sys_status}")
+def check_instance_status():
+    statuses = ec2_client.describe_instance_status(
+        IncludeAllInstances=True
+    )
+    for status in statuses["InstanceStatuses"]:
+        ins_status = status["InstanceStatus"] ["Status"]
+        sys_status = status["SystemStatus"]["Status"]
+        state = status["InstanceState"]["Name"]
+        print(f"Instance {status['InstanceId']} is {state} status is {ins_status} and system status is {sys_status}")
+     print("###################################################\n")
+
+
+schedule.every(5).seconds.do(check_instance_status)
+
+while True:
+    schedule.run_pending()
